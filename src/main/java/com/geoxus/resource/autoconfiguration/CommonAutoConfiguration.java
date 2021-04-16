36a8596729f861c76337417ceb1e5e747b4641f1
britton <britton@126.com>
2021-04-16 13:19:32 +0800

    完善模板代码

--- src/main/java/com/geoxus/resource/autoconfiguration/CommonAutoConfiguration.java
+++ src/main/java/com/geoxus/resource/autoconfiguration/CommonAutoConfiguration.java
@@ -1,8 +1,7 @@
 package com.geoxus.resource.autoconfiguration;
 
-import com.geoxus.resource.autoconfiguration.annotations.CommonResourceAnnotation;
 import com.geoxus.resource.autoconfiguration.annotations.CommonAnnotation;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.geoxus.resource.autoconfiguration.annotations.CommonResourceAnnotation;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -10,23 +9,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationUtils;
 
+import javax.annotation.Resource;
 import java.lang.reflect.Method;
 import java.util.Map;
 
 @Configuration
 @EnableConfigurationProperties(CommonProperties.class)
 public class CommonAutoConfiguration {
-    @Autowired
+    @Resource
     private ApplicationContext applicationContext;
 
     @Bean
     @ConditionalOnBean(name = "sendRequestToCloudService")
     public void init() {
-        System.out.println("Hello SSSSSS");
         Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(CommonAnnotation.class);
         for (String key : beansWithAnnotation.keySet()) {
             Class<?> aClass = beansWithAnnotation.get(key).getClass();
-            //获取指定bean的注解为ProductServiceAnnotation.class的注解属性
+            // 获取指定bean的注解为ProductServiceAnnotation.class的注解属性
             CommonAnnotation serviceAnnotation =
                     AnnotationUtils.findAnnotation(aClass, CommonAnnotation.class);
             System.out.println(serviceAnnotation);
--- src/main/java/com/geoxus/service/impl/SendRequestToCloudServiceImpl.java
+++ src/main/java/com/geoxus/service/impl/SendRequestToCloudServiceImpl.java
@@ -1,13 +1,20 @@
 package com.geoxus.service.impl;
 
 import com.geoxus.service.SendRequestToCloudService;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class SendRequestToCloudServiceImpl implements SendRequestToCloudService {
+    @Resource
+    private ApplicationContext applicationContext;
+
     @Override
     public String sayHello(String str) {
+        String applicationName = applicationContext.getApplicationName();
         System.out.println("我是资源收集器");
-        return "枫叶思源 : " + str;
+        return "枫叶思源 : " + str + " : " + applicationName;
     }
 }
